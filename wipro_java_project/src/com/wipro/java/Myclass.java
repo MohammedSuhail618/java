acf47190b273b85c83eb4ef0fc186e3d4c8e55df
MohammedSuhail618 <suhailshaik2024@gmail.com>
2025-02-01 17:42:19 +0530

    day1

---
+++ wipro_java_project/src/com/wipro/java/Myclass.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+class Myclass {
+	int num=5;
+
+	/**
+	 * 
+	 */
+	public void changeValue(int num) {
+		this.num = num;
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Myclass obj = new Myclass();//creating obj
+		System.out.println(obj.num);// calling function
+
+	}
+
+}
