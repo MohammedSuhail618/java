bbdbe0729970b05621b06aee8ef8e60823b1ac75
MohammedSuhail618 <suhailshaik2024@gmail.com>
2025-02-03 12:42:58 +0530

    Day2

--- wipro_java_project/src/com/wipro/java/oops/Developer.java
+++ wipro_java_project/src/com/wipro/java/oops/Developer.java
@@ -8,5 +8,18 @@ public class Developer extends employee {
 	public Developer() {
 		// TODO Auto-generated constructor stub
 	}
+	public static void main(String a[])
+	{
+		//child class is initiated from child constructor
+		//parent class consumes the properties of child class
+		employee employee=new Developer();
+		employee.setEmpId(100);
+		employee.setEmpName("Ramesh");
+		employee.setEmpAge(45);
+		
+		System.out.println(employee.getEmpId());
+		System.out.println(employee.getEmpName());
+		System.out.println(employee.getEmpAge());
+	}
 
 }
--- wipro_java_project/src/com/wipro/java/oops/Projectlead.java
+++ wipro_java_project/src/com/wipro/java/oops/Projectlead.java
@@ -9,5 +9,16 @@ public class Projectlead extends employee{
 	public Projectlead() {
 		// TODO Auto-generated constructor stub
 	}
-
+	public static void main(String a[])
+	{
+		//child class is initiated from child constructor
+		//parent class consumes the properties of child class
+		employee employee = new Projectlead();
+		employee.setEmpId(123);
+		employee.setEmpName("Yash");
+		employee.setEmpAge(34);
+		System.out.println(employee.getEmpId());
+		System.out.println(employee.getEmpName());
+		System.out.println(employee.getEmpAge());
+	}
 }
