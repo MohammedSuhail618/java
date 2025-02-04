c95a258947b05bab8a3105d888aaade02081095f
MohammedSuhail618 <suhailshaik2024@gmail.com>
2025-02-04 15:26:11 +0530

    Day3

---
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/practice/Circle.java
@@ -0,0 +1,8 @@
+package com.wipro.java.oops.polymorphism.practice;
+
+public class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
